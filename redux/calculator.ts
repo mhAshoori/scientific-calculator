ef9112ed89ae587a032665ea4d3d50a173197d49
Mohammad Hossein Ashoori <mh_ashoori@yahoo.com>
2022-02-25 13:05:01 +0330

    add "expre" state and reducers to "calculator.ts"

--- redux/calculator.ts
+++ redux/calculator.ts
@@ -1,11 +1,13 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-
+import {evaluate} from "mathjs"
 export interface CalculatorState {
-  ans: string | null;
+  ans: string | number | null;
+  expr: string | null;
 }
 
 const initialState: CalculatorState = {
   ans: null,
+  expr: null,
 };
 
 export const calculatorSlice = createSlice({
@@ -18,13 +20,32 @@ export const calculatorSlice = createSlice({
     decrement: (state) => {
       state.ans && (state.ans -= 1);
     }, */
-    addToAns: (state, action: PayloadAction<string | number>) => {
-      state.ans && (state.ans += action.payload);
+    addToExpr: (state, action: PayloadAction<string>) => {
+      if (!state.expr) {
+        state.expr = action.payload;
+      } else {
+        state.expr && (state.expr += action.payload.toString());
+      }
+    },
+    deleteFromExpr: (state) => {
+      if (state.expr) {
+        state.expr = state.expr.slice(0, -1);
+      }
+    },
+    clearExpr: (state) => {
+      if (state.expr) {
+        state.expr = null;
+      }
+    },
+    evalAns: (state) => {
+      if (state.expr) {
+        state.ans = evaluate(state.expr);
+      }
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { addToAns } = calculatorSlice.actions;
+export const { addToExpr, deleteFromExpr, clearExpr,evalAns } = calculatorSlice.actions;
 
 export default calculatorSlice.reducer;
