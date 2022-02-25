7d3fb12c467961468d4de6cb2b2e0a3e9d03514f
Mohammad Hossein Ashoori <mh_ashoori@yahoo.com>
2022-02-25 13:07:05 +0330

    add buttons.ts to data folder

---
+++ data/keys/buttons.ts
@@ -0,0 +1,52 @@
+/* import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
+import { FaSquareRootAlt } from "react-icons/fa"; */
+export const numButtons = {
+  numbers: [
+    { name: "one", value: 1 },
+    { name: "two", value: 2 },
+    { name: "three", value: 3 },
+    { name: "four", value: 4 },
+    { name: "five", value: 5 },
+    { name: "six", value: 6 },
+    { name: "seven", value: 7 },
+    { name: "eight", value: 8 },
+    { name: "nine", value: 9 },
+  ],
+  operands: [
+    { name: "delete", value: "DEL" },
+    { name: "AC", value: "AC" },
+    { name: "plus", value: "+" },
+    { name: "minus", value: "-" },
+    { name: "multiply", value: "*" },
+    { name: "divide", value: "/" },
+  ],
+  exes: [
+    { name: "zero", value: 0 },
+    { name: "dot", value: "." },
+    { name: "EXP", value: "EXP" },
+    { name: "Ans.", value: "ANS" },
+    { name: "equals.", value: "=" },
+  ],
+};
+
+export const funcButtons = [
+  { name: "SHIFT", value: "shift" },
+  { name: "ALPHA", value: "alpha" },
+  { name: "CALC", value: "calc" },
+  /*{ name: "left", value: "<BsArrowLeft />" },
+  { name: "right", value: "<BsArrowRight />" },
+  { name: "mode", value: "MODE" },
+  { name: "2nd", value: "2nd" },
+  { name: "integral", value: "<div>&#8747;</div>" },
+  { name: "up", value: "<AiFillCaretUp />" },
+  { name: "down", value: "<AiFillCaretDown />" },
+  { name: "down", value: "<AiFillCaretDown />" }, */
+  { name: "SQRT", value: "sqrt" },
+  { name: "^", value: "^" },
+  { name: "SIN", value: "sin" },
+  { name: "COS", value: "cos" },
+  { name: "TAN", value: "tan" },
+  { name: "(", value: "(" },
+  { name: ")", value: ")" },
+];
