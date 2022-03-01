02d8410d5419815fdbbeb7a76708ab131fd7828d
Mohammad Hossein Ashoori <mh_ashoori@yahoo.com>
2022-03-01 17:06:38 +0330

    modify "calculator" reducer

--- redux/calculator.ts
+++ redux/calculator.ts
@@ -1,13 +1,18 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import {evaluate} from "mathjs"
+import { stat } from "fs";
+import math , { evaluate } from "mathjs";
+
+//const mathJax = require('mathjax')
 export interface CalculatorState {
   ans: string | number | null;
-  expr: string | null;
+  expr: string[] | null;
+  texExpOutput: any;
 }
 
 const initialState: CalculatorState = {
   ans: null,
   expr: null,
+  texExpOutput: null,
 };
 
 export const calculatorSlice = createSlice({
@@ -21,15 +26,17 @@ export const calculatorSlice = createSlice({
       state.ans && (state.ans -= 1);
     }, */
     addToExpr: (state, action: PayloadAction<string>) => {
-      if (!state.expr) {
-        state.expr = action.payload;
+      if (!state.expr?.length) {
+        state.expr = [];
+        state.expr.push(action.payload);
       } else {
-        state.expr && (state.expr += action.payload.toString());
+        state.expr && state.expr.push(action.payload.toString());
       }
     },
     deleteFromExpr: (state) => {
       if (state.expr) {
-        state.expr = state.expr.slice(0, -1);
+        //state.expr = state.expr.slice(0, -1);
+        state.expr.pop();
       }
     },
     clearExpr: (state) => {
@@ -39,13 +46,27 @@ export const calculatorSlice = createSlice({
     },
     evalAns: (state) => {
       if (state.expr) {
-        state.ans = evaluate(state.expr);
+        state.ans = evaluate(state.expr.join(""));
       }
     },
+    texExpMaker: (state) => {
+    /*   require("mathjax")
+  .init({
+    loader: { load: ["input/tex", "output/svg"] },
+  })
+  .then((MathJax:any) => {
+    const svg = MathJax.tex2svg("\\frac{1}{x^2-1}", { display: true });
+    console.log(MathJax.startup.adaptor.outerHTML(svg));
+    state.texExpOutput = svg
+  })
+  .catch((err:any) => console.log(err.message));
+       */
+    }
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { addToExpr, deleteFromExpr, clearExpr,evalAns } = calculatorSlice.actions;
+export const { addToExpr, deleteFromExpr, clearExpr, evalAns,texExpMaker } =
+  calculatorSlice.actions;
 
 export default calculatorSlice.reducer;
